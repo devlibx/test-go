32a790ba62e2485986523171cce73b6d5945604c
Harish Bohara <harish.bohara@gmail.com>
2024-05-26 15:56:24 +0530

    test

--- v2/helper/helper.go
+++ v2/helper/helper.go
@@ -3,5 +3,5 @@ package helper
 import "github.com/devlibx/test-go/v2/infra"
 
 func Foo() string {
-	return "Version v2.0.3 - " + infra.GetMe()
+	return "Version v2.0.3 - " + infra.GetMe("This is go 21")
 }
--- v2/infra/infra.go
+++ v2/infra/infra.go
@@ -1,5 +1,11 @@
 package infra
 
-func GetMe() string {
-	return "v2.0.3-Infra"
+import "fmt"
+
+type Input interface {
+	string
+}
+
+func GetMe[T Input](v T) string {
+	return fmt.Sprintf("v2.0.3-Infra-v--%s", v)
 }
